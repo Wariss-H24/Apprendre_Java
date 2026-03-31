c56a360c9dbafaa96d5de4a7a9a7fd5831ece93f
Wariss Achamou Adeniyi BADA <wariss.bada@chem.loc>
2026-03-31 17:59:32 +0100

    Niveau 2 j'usqu'excercice 6

--- exo_base/learn.java
+++ exo_base/learn.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class learn {
     public static void main(String[] args) {
         myMethode();
@@ -8,9 +10,16 @@ public class learn {
         //Exo 2
         System.out.println("La somme des nombres est : " + Somme(nombres));
         //Exo 3
-        
         int[] num = {3, 7, 2, 9, 5};
         System.out.println("Le plus grand des nombres est : " + MaxNumber(num));
+        //Exo 4
+        System.out.println("Le tableau contient : " + PairNumber(num) + "nombres pair");
+
+        //Niveau 2
+        //Exo 5
+        System.out.println("La moyenne des elements est : " + moyenne(num));
+        //Exo 6
+        System.out.println("L'inverse du tableau est : " + Arrays.toString(inverse(num)));
     }
     
     //Hello world
@@ -43,5 +52,36 @@ public class learn {
             }
         }
         return max;
+    }
+
+    //Funcion exo 4
+
+    public static int PairNumber(int[] pair){
+        int getPair=0;
+        for ( int i = 0; i < pair.length; i++){
+            if (pair[i] % 2 == 0){
+                getPair++;
+            }
+        }
+        return getPair;
+    }
+
+
+    //Niveau 2
+    //Function exo 5
+    public static double moyenne(int[] tab){
+        int MyMoy = 0;
+        for(int i=0; i < tab.length ; i++){
+            MyMoy += tab[i];
+        }
+        return (double) MyMoy / tab.length;
+    }
+
+    public static int[] inverse(int[] bat){
+        int[] result= new int[bat.length];
+        for (int i =0; i < bat.length; i++){
+           result[i] = bat[bat.length - 1 - i];
+        }
+        return result;
     }
 }
